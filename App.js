016207bbb654cba3b70878a41ed2bd8a7bbb76b6
Jean Carla Guarra <jean.carla.guarra@gmail.com>
2023-03-06 00:10:37 +0800

    activity main

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image} from 'react-native';
+import styles from './Style';
+import Buttons from './Landing-Page/Google-Button'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+  source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1245px-Twitter-logo.svg.png'}}
+  style={{width: 40, height: 33}}
+/>
+<Text style= {styles.text}>
+  See what’s happening in the world right now.
+</Text>
+<Buttons  style ={styles.button}
+  title="Press me" onPress={() => 
+  alert('Button pressed')} />
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Style.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        //backgroundColor: 'red',
+        alignItems: 'center',
+        padding: 50 ,
+      },
+    text: {
+
+      backgroundColor: 'green',
+      marginTop: 200,
+      position: 'absolute',
+      width: 300,
+      height: 150,
+      fontFamily:'',
+      fontSize: 36,
+      color: 'black',
+      fontWeight: 600,
+    },
+    button: {
+      backgroundColor: 'orange',
+      padding: 50,
+      marginTop:100,
+      borderRadius:8,
+      borderStyle: 'solid',
+    }
+  });
